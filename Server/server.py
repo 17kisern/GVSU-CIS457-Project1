06fbc415cba0108915cd29a58bb31ad02bf5d1c9
Stephen Kiser <17kisern@gmail.com>
2020-10-26 18:35:14 -0400

    Everything is fucking done

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,5 @@
 import os
+from os import path
 import socket                   # Import socket module
 import asyncio
 
@@ -26,31 +27,88 @@ asyncio is just a library that allows us to run parallel operations without stre
 
 
 def List(connection, commandArgs):
-    return
+    global bufferSize
 
-def Retrive(connection, commandArgs):
     connectionAddress = connection[1]
     connectionSocket = connection[0]
 
-    fileName = "OriginalFile.txt"
-    fileItself = open(fileName, "rb")
+    connectionSocket.send(b"\nFiles on Server: \n")
+
+    for fileFound in os.listdir("."):
+        joiner = ""
+        connectionSocket.send(joiner.join([" - ", fileFound, "\n"]).encode("UTF-8"))
+
+    connectionSocket.send(b"\0")
+    return
+
+
+def Retrieve(connection, commandArgs):
+    connectionAddress = connection[1]
+    connectionSocket = connection[0]
+
+    # Sending status code for if the file exists
+    fileName = commandArgs[1]
+    try:
+        fileItself = open(fileName, "rb")
+        connectionSocket.send("200".encode("UTF-8"))
+    except:
+        connectionSocket.send("300".encode("UTF-8"))
+        return
 
     # Breaking the file down into smaller data chunks
     fileInBytes = fileItself.read(bufferSize)
 
     while fileInBytes:
         connectionSocket.send(fileInBytes)
-        print("[", connectionAddress, "] Sent: ", fileInBytes.decode("utf-8"))
 
+        # Reading in the next chunk of data
         fileInBytes = fileItself.read(bufferSize)
 
+    print("[", connectionAddress, "] Sent: ", commandArgs[1])
+
     # Let the client know we're done sending the file
-    connectionSocket.send(b'\0')
+    connectionSocket.send(b"\0")
     fileItself.close()
     return
 
+
 def Store(connection, commandArgs):
+    global bufferSize
+
+    connectionAddress = connection[1]
+    connectionSocket = connection[0]
+
+    try:
+        joiner = ""
+        receivedFile = open(commandArgs[1], 'wb')
+    except:
+        print("[", connectionAddress, "] Error in downloading file")
+        return
+
+    reachedEOF = False
+
+    while not reachedEOF:
+        print("[", connectionAddress, "] Downloading file from client...")
+
+        # Receiving data in 1 KB chunks
+        data = connectionSocket.recv(bufferSize)
+        if(not data):
+            reachedEOF = True
+            break
+
+        # If we reached the end of the file in the latest chunk, then break out of our loop
+        decodedString = data.decode("UTF-8")
+        if(len(decodedString) >= 2 and decodedString[len(decodedString) - 1: len(decodedString)] == "\0"):
+            reachedEOF = True
+            decodedString = decodedString[0: len(decodedString) - 1]
+
+        # Write data to a file
+        receivedFile.write(data)
+
+    receivedFile.close()
+    print("[", connectionAddress, "] Successfully downloaded and saved: ", commandArgs[1])
     return
+
 
 def ShutdownConnection(connection):
     global activeConnections
@@ -64,6 +122,7 @@ def ShutdownConnection(connection):
     connectionSocket.close()
     activeConnections.remove(connection)
 
+
 def ShutdownServer():
     global activeConnections
     global queueShutdown
@@ -72,6 +131,7 @@ def ShutdownServer():
         ShutdownConnection(connection)
     queueShutdown = True
     return
+
 
 async def ManageConnection(connection):
     connectionAddress = connection[1]
@@ -85,36 +145,38 @@ async def ManageConnection(connection):
 
         print("[", connectionAddress, "] Received Command: ", commandGiven)
 
-        if(commandGiven.upper() == "LIST" and len(commandArgs) == 1):
+        if(len(commandArgs) == 1 and commandArgs[0].upper() == "LIST"):
             List(connection, commandArgs)
             continue
-        elif(commandGiven.upper() == "RETRIEVE"):
+        elif(len(commandArgs) == 2 and commandArgs[0].upper() == "RETRIEVE"):
             Retrieve(connection, commandArgs)
             continue
-        elif(commandGiven.upper() == "STORE"):
+        elif(len(commandArgs) == 2 and commandArgs[0].upper() == "STORE"):
             Store(connection, commandArgs)
             continue
-        elif(commandGiven.upper() == "QUIT" or commandGiven.upper() == "DISCONNECT"):
+        elif(len(commandArgs) == 1 and (commandArgs[0].upper() == "QUIT" or commandArgs[0].upper() == "DISCONNECT")):
             # Close this connection
             ShutdownConnection(connection)
             break
-        elif(commandGiven.upper() == "SHUTDOWN_SERVER"):
+        elif(len(commandArgs) == 1 and commandArgs[0].upper() == "SHUTDOWN_SERVER"):
             ShutdownConnection(connection)
             ShutdownServer()
             break
         else:
             print("Invalid Command Received.")
+            print("Received ", len(commandArgs), " arguments.\nCommand: ", commandGiven)
             connectionSocket.send(b"Invalid Command. Please Try Again.")
             connectionSocket.send(b"\0")
             continue
 
 
 def Main():
+    global activeConnections
+
     # Create a socket object
     openSocket = socket.socket()
 
     # Get local machine name
-    # host = socket.gethostname()
     host = "localhost"
 
     # Bind to the port
@@ -130,11 +192,15 @@ def Main():
         print("Awaiting Connection")
         if not queueShutdown:
             tupleboi = openSocket.accept()
-            global activeConnections
-            activeConnections.append(tupleboi)
-            asyncio.run(ManageConnection(tupleboi))
+            if(len(activeConnections) < maxConnections):
+                tupleboi[0].send(b"200")
+                activeConnections.append(tupleboi)
+                asyncio.run(ManageConnection(tupleboi))
+            else:
+                tupleboi[0].send(b"300")
         else:
             break
+
 
 Main()
 print("Program Closing")
